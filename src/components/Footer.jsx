36c401b37d89df41f8ed8f9ed2ca923464720da7
AlinePaulinoMartins <alinepama15@gmail.com>
2021-12-03 22:10:36 -0300

    tentativa de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa";
+import Foguete from "../images/rocket.png"
 
 const TesteFooter = styled.div`
-    background-color: #83AEFB;
-    bottom: 0;
-    /* position: absolute; */
-    width:100%;
-    display: flex;
-    align-itens: center;
-    justify-content: center;
-    color: #fff;
+background-color: #83AEFB;
+color: #ffffff;
+display:flex;
+justify-content: space-between;
+align-items: center;
+height: 4rem;
+padding: 0rem 3rem;
 `
+const EstiloFoguete = styled.img`
+margin:10px 10px;
+max-width: 50px;
+`
+const EstiloSocial = styled.li`
+display: inline-block;
+margin: 8px;
+font-size: 25px;
+`
+
 
 export class Footer extends React.Component {
 
     render() {
         return(
             <TesteFooter>
+                <EstiloFoguete src= {Foguete}/>
                 <p>Criado por: Aline Paulino, Luana Fraporti e Pedro sekine</p>
-                <ul>
-                    <li>
+                <EstiloSocial>
                         <a href="https://pt-br.facebook.com/login.php">
                         <FaFacebook/>
                         </a>
-                    </li>
-                    <li>
+
                         <a href="https://www.instagram.com/">
                         <FaInstagram/>
                         </a>
-                    </li>
-                    <li>
+                
                         <a href="https://br.linkedin.com/">
                         <FaLinkedin/>
                         </a>
-                    </li>
-                </ul>
+                </EstiloSocial>
             </TesteFooter>
         )
     }
